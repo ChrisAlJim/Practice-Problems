67de776d2807cebe1283ca57e9cb7bd69133b3d3
ChrisAlJim <jimenezaot@gmail.com>
2025-06-28 22:20:00 -0400

    LeetCode 226 Invert Binary Trees

---
+++ LeetCode/226-Invert-Binary-Tree.js
@@ -0,0 +1,20 @@
+/*
+Given the root of a binary tree, invert the tree, and return its root.
+*/
+
+var invertTree = function(node) {
+    if (!node) return node
+    const stack = [node]
+
+    while (stack.length) {
+        const cur = stack.pop()
+        if (!cur) continue;
+        [cur.left, cur.right] = [cur.right, cur.left]
+        stack.push(cur.left, cur.right)
+    }
+    return node
+};
+
+/*
+
+*/
